b3b5553cc2e1dbfd6db82c51c7c1437adce91cd9
Harsh Kumbhani <Harshwut@icloud.com>
2024-01-18 17:36:09 +0100

    made a cross with red color

--- src/cub3d.c
+++ src/cub3d.c
@@ -6,7 +6,7 @@
 /*   By: hkumbhan <hkumbhan@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/13 17:52:22 by fgabler           #+#    #+#             */
-/*   Updated: 2024/01/18 17:11:19 by hkumbhan         ###   ########.fr       */
+/*   Updated: 2024/01/18 17:29:31 by hkumbhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,27 @@ int	main(int ac, char **av)
 {
 	(void) ac;
 	(void) av;
+	int x = -1;
+	int	y = -1;
 
+	mlx_t	*mlx;
+
+	mlx = mlx_init(WIDTH, HEIGHT, "Window", true);
+	mlx_image_t	*image;
+
+	image = mlx_new_image(mlx, WIDTH, HEIGHT);
+	mlx_image_to_window(mlx, image, x, y);
+	while (++y < HEIGHT)
+	{
+		x = -1;
+		while (++x < WIDTH)
+		{
+			if (x == y || (x + y == HEIGHT))
+				mlx_put_pixel(image, x, y, 0xFF0000FF);
+		}
+	}
+	//mlx_put_pixel(image, 0, 0, 0xFF0000FF);
+	mlx_loop(mlx);
+	mlx_terminate(mlx);
 	return (0);
 }
