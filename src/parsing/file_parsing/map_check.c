06059bcdfdaaab3d95b7207116c75977e103d083
Fritz Gäbler <58051701+fritz-gabler@users.noreply.github.com>
2024-01-24 16:25:41 +0100

    add map_check function

--- src/parsing/file_parsing/map_check.c
+++ src/parsing/file_parsing/map_check.c
@@ -1,8 +1,29 @@
 #include "cub3d.h"
 
-void	map_parsing(t_parsing *parser)
+static void	set_map_status(t_parsing *parser);
+static void	new_line_in_map(t_parsing *parser);
+
+void	map_check(t_parsing *parser)
 {
-	if (is_map_indicator(parser) == false || parser->error_occurred == true)
+	set_map_status(parser);
+	if (parser->map[start] == false || parser->error_occurred == true)
 		return ;
 	all_indicators_got_found(parser);
+	new_line_in_map(parser);
+}
+
+static void	set_map_status(t_parsing *parser)
+{
+	if (is_map_indicator(parser) == true && parser->map[start] == false)
+		parser->map[start] = true;
+	else if (parser->map[start] == true && parser->line[0] == '\n')
+		parser->map[end] = true;
+}
+
+static void	new_line_in_map(t_parsing *parser)
+{
+	if (parser->map[end] == true && is_map_indicator(parser) == true)
+	{
+		parser->error_occurred = true; //print_error
+	}
 }
