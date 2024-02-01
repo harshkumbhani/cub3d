59ed5483d808c87ea6e0fbcd518803d78a237d61
fritz-gabler <fritzlmgaebler@gmail.com>
2024-02-01 20:15:25 +0100

    add setting up input struct

--- src/input_handling/set_up_structs.c
+++ src/input_handling/set_up_structs.c
@@ -2,12 +2,14 @@
 
 static void	get_input(t_parsing *parser, int ac, char **av);
 static void	get_color_struct(t_parsing *parser);
+static void	get_texture_struct(t_parsing *parser);
 
 void	set_up_structs(t_parsing *parser, int ac, char **av)
 {
 	ft_bzero(parser, sizeof(t_parsing));
 	get_input(parser, ac, av);
 	get_color_struct(parser);
+	get_texture_struct(parser);
 }
 
 static void	get_input(t_parsing *parser, int ac, char **av)
@@ -25,6 +27,16 @@ static void	get_color_struct(t_parsing *parser)
 	static t_color floor_struct;
 	static t_color ceiling_struct;
 
+	ft_bzero(&floor_struct, sizeof(t_color));
+	ft_bzero(&ceiling_struct, sizeof(t_color));
 	parser->color[floor] = &floor_struct;
 	parser->color[ceiling] = &ceiling_struct;
+}
+
+static void	get_texture_struct(t_parsing *parser)
+{
+	static t_directions	texture;
+
+	ft_bzero(&texture, sizeof(t_directions));
+	parser->input->texture = &texture;
 }
