c5c41b8f978f06b8230b13f85e7476c3534cce86
Fritz Gäbler <fritzlmgaebler@gmail.com>
2024-02-06 20:05:20 +0100

    add hot fix need to doubble check

--- src/map_manipulation/enum_map_allocation.c
+++ src/map_manipulation/enum_map_allocation.c
@@ -8,7 +8,7 @@ void	enum_map_allocation(t_game *game, t_parsing *parser)
 
 	get_number_of_map_rows(parser->input->map, &number_of_rows);
 	game->map = (t_map_content **)
-		malloc((number_of_rows + 1) * sizeof(t_map_dimensions *));
+		malloc((number_of_rows + 2) * sizeof(t_map_dimensions *));
 	if (game->map == NULL)
 		return (parser_error(MEMORY_FAILED, NULL, parser));
 	set_y_axis_null(game->map, number_of_rows + 1);
