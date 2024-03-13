1a7bfe6a41c5019ca00a56d19a092533ce52ef1e
Fritz Gäbler <fritzlmgaebler@gmail.com>
2024-03-13 14:38:00 +0100

    add a condition that checks for \0 in written file

--- src/parsing/file_parsing/map_line_check.c
+++ src/parsing/file_parsing/map_line_check.c
@@ -1,5 +1,6 @@
 #include "cub3d.h"
 
+static int	run_while_loop(char c, t_parsing *parser);
 static void	is_map_component_check(char c, t_parsing *parser);
 static void	player_check(char c, t_parsing *parser);
 
@@ -10,7 +11,7 @@ void	map_line_check(t_parsing *parser)
 	if (parser->error_occurred == true || is_map_indicator(parser) == false)
 		return ;
 	tmp_line = parser->line;
-	while (*tmp_line != '\n' && parser->error_occurred == false)
+	while (run_while_loop(*tmp_line, parser) == true)
 	{
 		is_map_component_check(*tmp_line, parser);
 		player_check(*tmp_line, parser);
@@ -34,4 +35,13 @@ static void	player_check(char c, t_parsing *parser)
 		parser->indicator->player++;
 	if (parser->indicator->player > 1)
 		return (parser_error(GREATER_TWO_PLAYER, parser->line, parser));
+}
+
+static int	run_while_loop(char c, t_parsing *parser)
+{
+	if (c != '\n'
+		&& parser->error_occurred == false
+		&& c != '\0')
+		return (true);
+	return (false);
 }
