7a56771d88a044103c96c3c58f0609d0e4155252
Harsh Kumbhani <Harshwut@icloud.com>
2024-03-13 18:27:13 +0100

    exit functions with norm confirm

--- src/exit/exit.c
+++ src/exit/exit.c
@@ -6,15 +6,31 @@
 /*   By: hkumbhan <hkumbhan@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 07:38:36 by hkumbhan          #+#    #+#             */
-/*   Updated: 2024/03/12 17:33:23 by hkumbhan         ###   ########.fr       */
+/*   Updated: 2024/03/13 16:06:53 by hkumbhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	free_execution(t_cub3d *mlx)
+void	free_execution(t_cub3d *cub3d)
 {
-	mlx_delete_image(mlx->mlx, mlx->image);
-	mlx_close_window(mlx->mlx);
-	mlx_terminate(mlx->mlx);
+	int			i;
+	t_parsing	**parser;
+
+	parser = get_parser_struct();
+	i = -1;
+	while (++i < 4)
+	{
+		if (cub3d->meta_data->direction_img[i] != NULL)
+			mlx_delete_texture(cub3d->meta_data->direction_img[i]);
+	}
+	if (cub3d->image != NULL)
+		mlx_delete_image(cub3d->mlx, cub3d->image);
+	if (cub3d->player_window != NULL)
+		mlx_delete_image(cub3d->mlx, cub3d->player_window);
+	if (cub3d->minimap != NULL)
+		mlx_delete_image(cub3d->mlx, cub3d->minimap);
+	mlx_close_window(cub3d->mlx);
+	mlx_terminate(cub3d->mlx);
+	free_structs(cub3d->meta_data, *parser);
 }
